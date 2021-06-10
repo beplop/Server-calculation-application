bc8ecd12c26d261ee43be60371e80875aa4a3037
beplop <56293559+beplop@users.noreply.github.com>
2021-06-10 20:00:21 +0300

    Add files via upload

---
+++ WpfGameApp/CalcRow.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfGameApp
+{
+    public class CalcRow
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+        public int CPU { get; set; }
+        public int Network { get; set; }
+        public int KVM { get; set; }
+        public double HDD { get; set; }
+    }
+}
